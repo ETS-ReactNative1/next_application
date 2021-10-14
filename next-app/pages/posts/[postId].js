68f0369b78d6a8fb956d833894105477a77478c1
Julian Parra <jparra0417@gmail.com>
2021-10-14 09:00:35 -0500

    Fallback explanation

--- next-app/pages/posts/[postId].js
+++ next-app/pages/posts/[postId].js
@@ -1,6 +1,11 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 const Post = ({ post }) => {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <h1>Loading...</h1>;
+  }
   return (
     <div>
       <h1>
@@ -18,6 +23,12 @@ export async function getStaticProps(context) {
   const url = `https://jsonplaceholder.typicode.com/posts/${params.postId}`;
   const response = await fetch(url);
   const post = await response.json();
+  if (!post.id) {
+    return {
+      notFound: true,
+    };
+  }
+  console.log("Generating page for postId: " + post.id);
   return {
     props: {
       post,
@@ -29,9 +40,14 @@ export async function getStaticPaths() {
   const url = `https://jsonplaceholder.typicode.com/posts`;
   const response = await fetch(url);
   const posts = await response.json();
-  const paths = posts.map((post) => ({ params: { postId: `${post.id}` } }));
+  // const paths = posts.map((post) => ({ params: { postId: `${post.id}` } }));
+  const paths = [
+    { params: { postId: "1" } },
+    { params: { postId: "2" } },
+    { params: { postId: "3" } },
+  ];
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 }
